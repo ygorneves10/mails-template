a7058f44774c8f310440a95761709e4988e61acd
Ygor Neves <ygor.oliveira@corebiz.com.br>
2019-08-09 13:54:45 -0300

    Fix: mail serve

--- mails.js
+++ mails.js
@@ -1,6 +1,8 @@
 var dir = require('node-dir');
 var fs = require('fs')
 var folder = "templates"
+var http = require('http');
+const files = []
 
 var deleteFolderRecursive = function (path) {
     if (fs.existsSync(path)) {
@@ -29,23 +31,39 @@ try {
     console.error(err)
 }
 
-dir.readFiles('mails',
-    function (err, content, filename, next) {
-        if (err) throw err;
-        const fileContent = header + content + footer
-        const newFilePath = './' + folder + "/" + filename.split("\\")[1]
-        try {
-            fs.appendFile(newFilePath, fileContent, err => {
-                if (err) {
-                    console.log(err)
-                }
-            });
-        } catch (e) {
-            console.log("Cannot append file ", e);
-        }
-        next();
-    },
-    function (err, files) {
-        if (err) throw err;
-        console.log('Finished reading files:', files);
+dir.readFiles('mails', (err, content, filename, next) => {
+    if (err) throw err;
+    const fileContent = header + content + footer
+    const newFilePath = './' + folder + "/" + filename.split("\\")[1]
+    files.push(newFilePath)
+    try {
+        fs.appendFile(newFilePath, fileContent, err => {
+            if (err) {
+                console.log(err)
+            }
+        });
+    } catch (e) {
+        console.log("Cannot append file ", e);
+    }
+    next();
+}, (err, files) => {
+    if (err) throw err;
+
+    http.createServer((req, res) => {
+        fs.readFile("templates/" + req.url, (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    }).listen(8080, () => {
+        files.forEach(file => {
+            console.log("Press CTRL + Click to open => http://localhost:8080/" + file.split("\\")[1])
+        })
+        console.log("")
+        console.log("Press CTRL + C to stop")
     });
+});
